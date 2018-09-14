cf124680385f976ea11a4447c04328996ea17d00
Alexander Connolly <alexanderconnolly@Alexanders-MacBook-Pro.local>
2018-09-13 21:22:01 -0700

    First

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Cheeseburger');
+INSERT INTO burgers (name) VALUES ('BBQ');
+INSERT INTO burgers (name, sleepy) VALUES ('Bacon', true);
+INSERT INTO burgers (name, sleepy) VALUES ('California', true);
+INSERT INTO burgers (name, sleepy) VALUES ('Veggie', true);
+INSERT INTO burgers (name) VALUES ('Southwest');
